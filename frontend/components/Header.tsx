8fb3ebe77245048717e6d5896fe66e7279886316
francisamidu <francisamidu124@gmail.com>
2023-01-13 18:52:36 +0200

    UI tweaks

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -5,20 +5,33 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="min-h-[96vh] flex flex-row items-center">
-      <div className="md:max-w-xl md:mx-auto">
+    <header className="min-h-[94vh] p-5">
+      <div className="md:max-w-screen-xl md:mx-auto flex flex-row items-center">
         <div className="w-2/5">
-          <Image src="/image.jpg" className="w-full" alt="Image here" />
+          <Image
+            src="/image.jpg"
+            className="rounded"
+            alt="Image here"
+            width="500"
+            height="500"
+          />
         </div>
         <div className="w-3/5 h-screen p-7 flex flex-col justify-center">
-          <h1 className="my-2 text-white text-2xl font-bold">{shared.name}</h1>
-          <h2 className="my-2 text-blue-gray-100">{shared.description}</h2>
+          <h1 className="my-2 text-blue-gray-700 text-2xl font-bold">
+            {shared.name}
+          </h1>
+          <h2 className="my-3 text-blue-gray-600">{shared.description}</h2>
           <form>
-            <div className="flex flex-row">
-              <Input placeholder="Enter your email address" className="w-3/5" />
-              <Button color="green">Subscribe Now</Button>
+            <div className="flex flex-row items-center">
+              <input
+                placeholder="Enter your email address"
+                className="w-3/5 p-2 rounded-lg border-blue-gray-50 border-[1px] mr-2"
+              />
+              <Button color="green" className="!w-max">
+                Subscribe Now
+              </Button>
             </div>
-            <span className="text-green-600 w-max">
+            <span className="text-green-600 mt-2">
               You have subscribed! Check your email for the confirmation
             </span>
           </form>
