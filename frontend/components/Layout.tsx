2493630433066091d3425bea2c95928bfd735cc8
francisamidu <francisamidu124@gmail.com>
2023-01-13 17:29:49 +0200

    Layout update

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,20 +1,18 @@
 import Head from "next/head";
 import React, { PropsWithChildren } from "react";
-import { Footer, Navbar } from ".";
+import { Footer } from ".";
 import shared from "../shared.json";
-import { LayoutProps } from "../types";
 const { name, description } = shared;
 
-const Layout = ({ children, loggedIn, page }: LayoutProps) => {
+const Layout = ({ children }: Partial<PropsWithChildren>) => {
   return (
     <>
       <Head>
-        <title>{`${page} - ${name}`}</title>
-        <meta name="title" content={`${page} | ${name}`}></meta>
+        <title>{`${name}-${description}`}</title>
+        <meta name="title" content={`${name}-${description}`}></meta>
         <meta name="description" content={description}></meta>
       </Head>
       <main className="flex flex-col justify-between min-h-screen">
-        <Navbar loggedIn={loggedIn} />
         {children}
         <Footer />
       </main>
