0b900acdf5fe9b00fa823e7e8ec3bff1b662a4eb
francisamidu <francisamidu124@gmail.com>
2023-01-13 17:43:28 +0200

    UI tweaks

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,16 +1,22 @@
+import { Button, Input } from "@material-tailwind/react";
 import React from "react";
 import shared from "../shared.json";
 
 const Header = () => {
   return (
-    <header className="header p-4 min-h-[96vh] flex flex-col justify-center">
-      <div className="overlay w-full h-full">
-        <h1 className="my-2 text-white text-center text-2xl font-bold">
-          {shared.name}
-        </h1>
-        <h2 className="my-2 text-blue-gray-100 text-center">
-          {shared.description}
-        </h2>
+    <header className="header min-h-[96vh]">
+      <div className="overlay w-full h-screen p-7 flex flex-col justify-center">
+        <h1 className="my-2 text-white text-2xl font-bold">{shared.name}</h1>
+        <h2 className="my-2 text-blue-gray-100">{shared.description}</h2>
+        <form>
+          <div className="flex flex-row">
+            <Input placeholder="Subscribe to animescraper" />
+            <Button color="green">Subscribe</Button>
+          </div>
+          <span className="text-green-600">
+            You have subscribed! Check your email for the confirmatio
+          </span>
+        </form>
       </div>
     </header>
   );
