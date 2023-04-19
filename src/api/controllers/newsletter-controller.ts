e5b502f8058d45f38b6f15683c01bdcc54439104
francisamidu <francisamidu124@gmail.com>
2023-04-19 11:15:42 +0200

    Added error handling to services

--- src/api/controllers/newsletter-controller.ts
+++ src/api/controllers/newsletter-controller.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { use } from "../../middleware";
 
 import { NewsletterService } from "../services";
 
@@ -6,10 +7,10 @@ const router = express.Router();
 
 router.get("/", NewsletterService.get);
 
-router.post("/confirm", NewsletterService.confirm);
+router.post("/confirm", use(NewsletterService.confirm));
 
-router.post("/subscribe", NewsletterService.subscribe);
+router.post("/subscribe", use(NewsletterService.subscribe));
 
-router.post("/unsubscribe", NewsletterService.unsubscribe);
+router.post("/unsubscribe", use(NewsletterService.unsubscribe));
 
 export default router;
